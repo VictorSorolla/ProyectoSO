4aab3f75feef652ef78f004a355f28ae7c7d6f65
JaskiratSingh377 <sjaskirat0@gmail.com>
2021-06-19 12:37:35 +0200

    Versión final generada por Jaskirat Singh

--- ProyectoSO/cliente/PlayerUI/Program.cs
+++ ProyectoSO/cliente/PlayerUI/Program.cs
@@ -1,9 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace WindowsFormsApplication1
+namespace ProyectoSO
 {
     static class Program
     {
@@ -15,7 +16,7 @@ namespace WindowsFormsApplication1
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new LoginForm());
         }
     }
 }
